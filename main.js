e9a23b234c790141839e5c61b0dbe9688a537f20
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-30 11:38:38 +0100

    adding functionality to flip only the card clicked instead of flipping all cards on click

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ $('i.fas').on('click', () => {
 })
 
 
-$(".item").on('click', () => {
-    $(".item").toggleClass("flipped")
-})
+const cards = document.querySelectorAll(".item");
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', () => {
+        cards[i].classList.toggle("flipped")
+    })
+}
